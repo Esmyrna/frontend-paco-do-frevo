24eb7197ee15e10e314facf249017214e11b3a5e
JoaoVNicacio <joaonicks99@gmail.com>
2024-05-21 23:30:22 -0300

    feat: cria base de estilização de listagem

--- src/pages/ListOfAssociations/ListOfAssociations.tsx
+++ src/pages/ListOfAssociations/ListOfAssociations.tsx
@@ -11,23 +11,7 @@ import InputFilterSearch from "../../components/SearchInput";
 import InputFilterCategory from "../../components/InputFilterCategory";
 import { EAssociationType } from "../../interfaces/enum";
 import { FilterItemsAssociations } from "../../interfaces/filterAssociations";
- 
-
-
-interface Item {
-    id: string
-    name: string;
-    foundationDate: string;
-    colors: string[];
-    associationType: string;
-    activeMembers: number;
-    isSharedWithAResidence: boolean;
-    hasOwnedHeadquarters: boolean;
-    isLegalEntity: boolean;
-    cnpj: string;
-    canIssueOwnReceipts: boolean;
-    associationHistoryNotes: string;
-}
+import { SimplifiedAssociationDTO } from "../../interfaces/type";
 
 interface IPagedResults<T> {
     result: Array<T>;
@@ -37,17 +21,17 @@ interface IPagedResults<T> {
     totalCount: number;
     totalPages: number;
 }
-
+''
 const ListOfAssociations = () => {
     const [currentPage, setCurrentPage] = React.useState<number>(1);
-    const [currentPageSize, setCurrentPageSize] = React.useState<number>(4);
+    const [currentPageSize, setCurrentPageSize] = React.useState<number>(5);
     const [items, setItems] = useState<FilterItemsAssociations[]>([]);
     const [search, setSearch] = useState<string>('');
     const [filterCategory, setFilterCategory] = useState<EAssociationType>();
 
     const BASE_URL = 'http://localhost:3000';
 
-    const fetchData = async (page: number, pageSize: number, search: string, filterCategory: EAssociationType): Promise<IPagedResults<Item>> => {
+    const fetchData = async (page: number, pageSize: number, search: string, filterCategory: EAssociationType): Promise<IPagedResults<SimplifiedAssociationDTO>> => {
         const response = await axios.get(`${BASE_URL}/associations/paged?pageSize=${pageSize}&page=${page}`, {
             params: {
                 searchParam: search,
@@ -75,20 +59,21 @@ const ListOfAssociations = () => {
         setSearch(event.target.value);
     };
     function separateCamelCase(word: string) {
+
         return word.replace(/([a-z])([A-Z])/g, '$1 $2');
     }
     return (
         <>
             <Nav />
             <C.Section>
-                <C.ContainerLeft>
+                {/* <C.ContainerLeft>
                     <C.ContainerPhone>
                         <C.ContainerDisplayPhone>
                             <SlideListOfAssociations />
                         </C.ContainerDisplayPhone>
                         <C.ButtonPhone>O</C.ButtonPhone>
                     </C.ContainerPhone>
-                </C.ContainerLeft>
+                </C.ContainerLeft> */}
                 <C.ContainerRight>
                     <C.ContainerFilterAssociations>
                         <C.SearchAssociation>
@@ -104,7 +89,7 @@ const ListOfAssociations = () => {
                                 placeholder="Filtrar por..."
                                 value={filterCategory}
                                 onChange={handleFilterCategoryChange}>
-                                <option value="">All</option>
+                                <option value="">Todos</option>
                                 <option value={EAssociationType.club}>Clube</option>
                                 <option value={EAssociationType.troca}>Troça</option>
                                 <option value={EAssociationType.carnivalBlock}>Bloco carnavalesco</option>
@@ -112,26 +97,49 @@ const ListOfAssociations = () => {
                             </InputFilterCategory>
                         </C.FilterAssociation>
                     </C.ContainerFilterAssociations>
+
                     <C.ContainerListing>
                         {data?.result && (
                             <C.Associations>
                                 {data.result.map(item => (
                                     <C.Association key={item.id}>
                                         <C.ContainerInfosAssociation>
-                                            <C.Title>Name:</C.Title>   {separateCamelCase(item.name)}
-                                            <C.Title>Data de fundação:</C.Title> {separateCamelCase(item.foundationDate)}
-                                            <C.Title>Data de fundação:</C.Title> {item.cnpj}
+                                            <C.AssociationPropertyBlock>
+                                                <C.Title>Nome:</C.Title>
+                                                {separateCamelCase(item.name)}
+                                            </C.AssociationPropertyBlock>
+
+                                            <C.AssociationPropertyBlock>
+                                                <C.Title>Tipo:</C.Title>
+                                                {separateCamelCase(item.associationType)}
+                                            </C.AssociationPropertyBlock>
+
+                                            <C.AssociationPropertyBlock>
+                                                <C.Title>Data de fundação:</C.Title>
+                                                {item.foundationDate
+                                                    ? new Date(item.foundationDate).toLocaleDateString('pt-BR') 
+                                                    : 'Data não disponível'}
+                                            </C.AssociationPropertyBlock>
+
+                                            <C.AssociationPropertyBlock>
+                                                <C.Title>Membros Ativos:</C.Title>
+                                                {item.activeMembers ?? '0'}
+                                            </C.AssociationPropertyBlock>
                                         </C.ContainerInfosAssociation>
                                     </C.Association>
                                 ))}
                             </C.Associations>
                         )}
-                        <button disabled={currentPage === 1} onClick={() => setCurrentPage((prev) => prev - 1)}>
-                            Página anterior
-                        </button>
-                        <button disabled={!data || !data.hasNextPage} onClick={() => setCurrentPage((prev) => prev + 1)}>
-                            Próxima página
-                        </button>
+
+                        <C.PageButtonsContainer>
+                            <button disabled={currentPage === 1} onClick={() => setCurrentPage((prev) => prev - 1)}>
+                                Página anterior
+                            </button>
+
+                            <button disabled={!data || !data.hasNextPage} onClick={() => setCurrentPage((prev) => prev + 1)}>
+                                Próxima página
+                            </button>
+                        </C.PageButtonsContainer>
                     </C.ContainerListing>
                 </C.ContainerRight>
             </C.Section>
--- src/pages/ListOfAssociations/style.ts
+++ src/pages/ListOfAssociations/style.ts
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import Fundo from '../../assets/Fundo.png';
 
 export const Section = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  background-image: url(${Fundo});
+  background-size: cover;
+  background-position: center;
+  justify-content: center;
 `;
 
 export const ContainerLeft = styled.div`
@@ -15,7 +20,18 @@ export const ContainerLeft = styled.div`
 `;
 
 export const ContainerRight = styled.div`
-  width: 70%;
+  width: 80%;
+`;
+
+export const PageButtonsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  justify-self: center;
+  align-self: flex-end;
+  gap: 2.5%;
 `;
 
 export const ContainerPhone = styled.div`
@@ -50,7 +66,14 @@ export const ContainerFilterAssociations = styled.div`
   display: flex;
 `;
 
-export const Associations = styled.ul``;
+export const Associations = styled.ul`
+  width: 95;
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Title = styled.p`
   margin: 0;
@@ -63,17 +86,24 @@ export const Association = styled.div`
   padding: 1rem;
   margin-bottom: 1rem;
   width: 90%;
+  background-color: #f5f5f5;
 `;
 
 export const ContainerInfosAssociation = styled.div`
   display: flex;
   padding: 1rem;
-  
-`;
+  gap: 10vw;
+  `;
 
 export const ContainerListing = styled.div`
   width: 100%;
-  height: 80%;
+  height: 70%;
+  background-color: #f5f5f5;
+  padding-top: 1%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  border-radius: 15px;
 `;
 
 export const SearchAssociation = styled.div`
@@ -104,4 +134,13 @@ export const SearchInput = styled.input`
   border: 2px solid #cccccc;
   border-radius: 10px;
   padding: 0.5rem;
+`;
+
+export const AssociationPropertyBlock = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+`;
+
+export const PagingButton = styled.button`
 `;
