56413f8a5bea2d0f652b7a17949497a0cac4d31b
DALuckwu <daraluckwu98@gmail.com>
2024-06-08 22:09:09 -0300

    removendo types de dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,4 @@
-import { useSearchParams } from "react-router-dom"
 import Nav from "../../components/Nav"
-import { useEffect, useState } from "react"
 import { VisualData } from "../../components/DashboardComponents/VisualData"
 import { DashboardContent, DashboardWrapper } from "./styles"
 
@@ -9,34 +7,12 @@ import { DashboardContent, DashboardWrapper } from "./styles"
  * @returns 
  */
 export const Dashboard = (): JSX.Element => {
-    const [searchParams, setSearchParams] = useSearchParams()
-
-    const [activePage] = useState(searchParams.get('activePage') || 'visualData')
-
-    const dashboardPages: DashboardPages = {
-        visualData: <VisualData />,
-        // allData: <AllData />,
-    }
-    const renderPage = () => {
-        switch (activePage) {
-            case 'visualData':
-                return <VisualData />
-            // case 'allData':
-            //     return <AllData />
-            default:
-                return null
-        }
-    }
-
-    useEffect(() => {
-        if (!activePage || !dashboardPages[activePage]) {
-            setSearchParams({ activePage: 'visualData'})
-        }
-    }, [])
     return (
         <DashboardWrapper>
             <Nav />
-            <DashboardContent>{renderPage()}</DashboardContent>
+            <DashboardContent>
+                <VisualData />
+            </DashboardContent>
         </DashboardWrapper>
     )
 }
--- src/pages/Dashboard/types.d.ts
+++
@@ -1,3 +0,0 @@
-interface DashboardPages {
-    [key: string]: JSX.Element // Índice de assinatura para permitir chaves de string
-}
